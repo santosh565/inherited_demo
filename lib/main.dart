d89e2589ba48fea8017fc1572fa73431ea6ca261
Santosh Bhattarai <hsotnas76@gmail.com>
2022-05-06 11:05:29 +0545

    provider demo begin

--- lib/main.dart
+++ lib/main.dart
@@ -19,8 +19,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-final sliderData = SliderData();
-
 class MyHomePage extends StatelessWidget {
   const MyHomePage({
     Key? key,
@@ -29,77 +27,9 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Default value'),
-      ),
-      body: SliderNotifier(
-        sliderData: sliderData,
-        child: Builder(builder: (context) {
-          return Column(
-            children: [
-              Slider(
-                  value: SliderNotifier.of(context),
-                  onChanged: (value) {
-                    sliderData.value = value;
-                  }),
-              Row(
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  Expanded(
-                    child: Opacity(
-                      opacity: SliderNotifier.of(context),
-                      child: Container(
-                        height: 150,
-                        color: Colors.yellow,
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Opacity(
-                      opacity: SliderNotifier.of(context),
-                      child: Container(
-                        height: 150,
-                        color: Colors.green,
-                      ),
-                    ),
-                  ),
-                ],
-              )
-            ],
-          );
-        }),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Default value'),
+        ),
+        body: const Text('provider'));
   }
-}
-
-class SliderData extends ChangeNotifier {
-  double _value = 0;
-  double get value => _value;
-
-  set value(double value) {
-    if (_value != value) {
-      _value = value;
-      notifyListeners();
-    }
-  }
-}
-
-class SliderNotifier extends InheritedNotifier<SliderData> {
-  const SliderNotifier({
-    Key? key,
-    required SliderData sliderData,
-    required Widget child,
-  }) : super(
-          key: key,
-          child: child,
-          notifier: sliderData,
-        );
-
-  static double of(BuildContext context) =>
-      context
-          .dependOnInheritedWidgetOfExactType<SliderNotifier>()
-          ?.notifier
-          ?.value ??
-      0.0;
 }
