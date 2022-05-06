2a689fa41f3fa3484326591f52d849e303754217
Santosh Bhattarai <hsotnas76@gmail.com>
2022-05-06 17:48:00 +0545

    multiprovider working demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 String currentDateTime() => DateTime.now().toIso8601String();
+
+@immutable
+class Seconds {
+  final String value;
+  Seconds() : value = currentDateTime();
+}
+
+@immutable
+class Minutes {
+  final String value;
+  Minutes() : value = currentDateTime();
+}
+
+
+
+class SecondsWidget extends StatelessWidget {
+  const SecondsWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final seconds = context.watch<Seconds>();
+    return Expanded(
+        child: Container(
+      height: 100,
+      color: Colors.yellow,
+      child: Text(seconds.value),
+    ));
+  }
+}
+
+class MinutesWidget extends StatelessWidget {
+  const MinutesWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final minutes = context.watch<Minutes>();
+    return Expanded(
+        child: Container(
+      height: 100,
+      color: Colors.blue,
+      child: Text(minutes.value),
+    ));
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -30,9 +75,33 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     debugPrint('homepage widget build');
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Default value'),
+      appBar: AppBar(
+        title: const Text('Default value'),
+      ),
+      body: MultiProvider(
+        providers: [
+          StreamProvider.value(
+            value: Stream<Seconds>.periodic(
+                const Duration(seconds: 1), (_) => Seconds()),
+            initialData: Seconds(),
+          ),
+          StreamProvider.value(
+            value: Stream<Minutes>.periodic(
+                const Duration(seconds: 5), (_) => Minutes()),
+            initialData: Minutes(),
+          ),
+        ],
+        child: Column(
+          children: [
+            Row(
+              children: const [
+                SecondsWidget(),
+                MinutesWidget(),
+              ],
+            )
+          ],
         ),
-        body: const Text('multiprovider'));
+      ),
+    );
   }
 }
