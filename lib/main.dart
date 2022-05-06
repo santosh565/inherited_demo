4db2dc27158eed86ed633d5ef5f0ad3f84c0997f
Santosh Bhattarai <hsotnas76@gmail.com>
2022-05-06 15:59:29 +0545

    example working

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:uuid/uuid.dart';
 
 void main() {
@@ -12,12 +13,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      create: (_) => ObjectProvider(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage(),
       ),
-      home: const MyHomePage(),
     );
   }
 }
@@ -84,6 +88,73 @@ class ObjectProvider extends ChangeNotifier {
   }
 }
 
+class ExpensiveWidget extends StatelessWidget {
+  const ExpensiveWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    debugPrint('expensive widget build');
+    final expensiveObject = context.select<ObjectProvider, ExpensiveObject>(
+        (provider) => provider.expensiveObject);
+    return Container(
+      color: Colors.blue,
+      height: 100,
+      child: Column(
+        children: [
+          const Text('Expensive Widget'),
+          Text(expensiveObject.id),
+          Text(expensiveObject.lastUpdated),
+        ],
+      ),
+    );
+  }
+}
+
+class CheapWidget extends StatelessWidget {
+  const CheapWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    debugPrint('cheap widget build');
+
+    final cheapObject = context.select<ObjectProvider, CheapObject>(
+        (provider) => provider.cheapObject);
+    return Container(
+      color: Colors.yellow,
+      height: 100,
+      child: Column(
+        children: [
+          const Text('Cheap Widget'),
+          Text(cheapObject.id),
+          Text(cheapObject.lastUpdated),
+        ],
+      ),
+    );
+  }
+}
+
+class ObjectProviderWidget extends StatelessWidget {
+  const ObjectProviderWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    debugPrint('object widget build');
+
+    final provider = context.watch<ObjectProvider>();
+    return Container(
+      color: Colors.purple,
+      height: 100,
+      child: Column(
+        children: [
+          const Text('Object Provider Widget'),
+          Text(provider.id),
+          // Text(provider.lastUpdated),
+        ],
+      ),
+    );
+  }
+}
+
 class MyHomePage extends StatelessWidget {
   const MyHomePage({
     Key? key,
@@ -91,10 +162,36 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    debugPrint('homepage widget build');
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Default value'),
-        ),
-        body: const Text('demo 2'));
+      appBar: AppBar(
+        title: const Text('Default value'),
+      ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.max,
+            children: const [
+              Expanded(child: ExpensiveWidget()),
+              Expanded(child: CheapWidget()),
+            ],
+          ),
+          const ObjectProviderWidget(),
+          TextButton(
+            onPressed: () {
+              context.read<ObjectProvider>().start();
+            },
+            child: const Text('start'),
+          ),
+          TextButton(
+            onPressed: () {
+              context.read<ObjectProvider>().stop();
+            },
+            child: const Text('stop'),
+          )
+        ],
+      ),
+    );
   }
 }
