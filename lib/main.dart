133abe39a92110821872bac75fba3fac07891261
Santosh Bhattarai <hsotnas76@gmail.com>
2022-05-08 12:26:09 +0545

    counter with provider

--- lib/main.dart
+++ lib/main.dart
@@ -5,47 +5,12 @@ void main() {
   runApp(const MyApp());
 }
 
-String currentDateTime() => DateTime.now().toIso8601String();
+class Counter with ChangeNotifier {
+  int count = 0;
 
-@immutable
-class Seconds {
-  final String value;
-  Seconds() : value = currentDateTime();
-}
-
-@immutable
-class Minutes {
-  final String value;
-  Minutes() : value = currentDateTime();
-}
-
-class SecondsWidget extends StatelessWidget {
-  const SecondsWidget({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final seconds = context.watch<Seconds>();
-    return Expanded(
-        child: Container(
-      height: 100,
-      color: Colors.yellow,
-      child: Text(seconds.value),
-    ));
-  }
-}
-
-class MinutesWidget extends StatelessWidget {
-  const MinutesWidget({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final minutes = context.watch<Minutes>();
-    return Expanded(
-        child: Container(
-      height: 100,
-      color: Colors.blue,
-      child: Text(minutes.value),
-    ));
+  void increment() {
+    count += 5;
+    notifyListeners();
   }
 }
 
@@ -59,7 +24,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: ChangeNotifierProvider(
+          create: (context) => Counter(), child: const MyHomePage()),
     );
   }
 }
@@ -72,32 +38,21 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          context.read<Counter>().increment();
+        },
+        child: const Text('data'),
+      ),
       appBar: AppBar(
         title: const Text('Default value'),
       ),
-      body: MultiProvider(
-        providers: [
-          StreamProvider.value(
-            value: Stream<Seconds>.periodic(
-                const Duration(seconds: 1), (_) => Seconds()),
-            initialData: Seconds(),
-          ),
-          StreamProvider.value(
-            value: Stream<Minutes>.periodic(
-                const Duration(seconds: 5), (_) => Minutes()),
-            initialData: Minutes(),
-          ),
-        ],
-        child: Column(
-          children: [
-            Row(
-              children: const [
-                SecondsWidget(),
-                MinutesWidget(),
-              ],
-            )
-          ],
-        ),
+      body: Consumer<Counter>(
+        builder: (_, value, __) {
+          return Text(
+            value.count.toString(),
+          );
+        },
       ),
     );
   }
